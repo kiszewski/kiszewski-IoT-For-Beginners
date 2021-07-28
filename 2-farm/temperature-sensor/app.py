294dde35496be089e563b337e269daf5a8a69b7b
Leonardo Kiszewski <kiszewski1999@gmail.com>
2021-07-27 21:29:23 -0300

    Send sensor read value via mqtt

--- 2-farm/temperature-sensor/app.py
+++ 2-farm/temperature-sensor/app.py
@@ -1,12 +1,24 @@
-from counterfit_connection import CounterFitConnection
-CounterFitConnection.init('127.0.0.1', 5000)
-
+import json
 import time
+import paho.mqtt.client as mqtt
+from configs import *
+from counterfit_connection import CounterFitConnection
 from counterfit_shims_seeed_python_dht import DHT
 
+CounterFitConnection.init('127.0.0.1', 5000)
 sensor = DHT("11", 5)
+
+client_name = mqtt_id + 'temp_sensor_client'
+mqtt_client = mqtt.Client(client_name)
+mqtt_client.connect('test.mosquitto.org')
+
+mqtt_client.loop_start()
 
 while True:
     humidity, temperature = sensor.read()
     print(f'Temperature {temperature}°C')
+    temp_json = json.dumps({'temperature': temperature})
+
+    mqtt_client.publish(temp_sensor_telemetry_topic, temp_json)
+
     time.sleep(10)
